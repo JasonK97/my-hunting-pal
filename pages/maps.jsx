a8031a9b6926b3cb09f3c20a52b5d8202ff05d56
Jason Kent <jasonbkent@gmail.com>
2023-02-21 14:48:09 -0700

    Pages added

---
+++ pages/maps.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { SEO } from '@/components'
+
+export default function Maps() {
+  return (
+    <>
+      <SEO 
+        page='Maps'
+        description='Take a look, map out and plan your hunting trip.'
+      />
+    </>
+  )
+}
