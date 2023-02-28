d7efce7004843bee00860962c280cfa35fd347e6
Jason Kent <jasonbkent@gmail.com>
2023-02-28 15:00:25 -0700

    map styling

--- pages/maps.jsx
+++ pages/maps.jsx
@@ -13,19 +13,52 @@ export default function Maps() {
     setPageIsMounted(true)
     const map = new mapboxgl.Map({
       container: 'starter-map',
-      style: 'mapbox://styles/mapbox/streets-v11',
+      // style: 'mapbox://styles/mapbox/streets-v11',
+      style: 'mapbox://styles/mapbox/satellite-streets-v12',
       center: [-96, 39],
       zoom: 4,
     });
 
-    map.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        trackUserLocation: true,
-      }),
-    );
+    map
+      .addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          trackUserLocation: true,
+        }),
+      )
+      .addControl(
+        new mapboxgl.NavigationControl()
+      )
+      .on('style.load', () => {
+        map.addSource('mapbox-dem', {
+          'type': 'raster-dem',
+          'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+          'tileSize': 512,
+          'maxzoom': 14
+        })
+        // add the DEM source as a terrain layer with exaggerated height
+        map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 2 })
+        map.setFog({
+          // 'range': [-4, 5],
+          'horizon-blend': 0,
+          'color': 'white',
+          'high-color': '#add8e6',
+          'space-color': '#d8f2ff',
+          'star-intensity': 0.0
+          })
+        // map.addLayer(
+        //   {
+        //     'id': 'hillshading',
+        //     'source': 'dem',
+        //     'type': 'hillshade'
+        //   },
+        //   // Insert below land-structure-polygon layer,
+        //   // where hillshading sits in the Mapbox Streets style.
+        //   'land-structure-polygon'
+        // )
+      })
 }, [])
 
   return (
