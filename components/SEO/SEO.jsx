ddbecc709724b857b2f507e6ee9ddcd63b391e8f
Jason Kent <jasonbkent@gmail.com>
2023-03-08 11:53:06 -0700

    SEO Head optimization

--- components/SEO/SEO.jsx
+++ components/SEO/SEO.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import Head from 'next/head'
+import NextHead from 'next/head'
 
 import { siteTitle } from './data'
 
 export const SEO = ({ page, description }) => {
+  const pageTitle = page || 'Page'
   const metaDescription = description || 'My Hunting Pal, your new companion for hunting trips.'
 
   return (
-    <Head>
-      <title>{page} | {siteTitle}</title>
+    <NextHead>
+      <title>{pageTitle} | {siteTitle}</title>
       <meta name="description" content={metaDescription} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/static/favicon.ico" />
-    </Head>
+    </NextHead>
   )
 }
